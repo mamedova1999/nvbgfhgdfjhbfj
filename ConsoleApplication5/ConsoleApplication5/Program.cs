6cd7ad067594bd049f179401f2663b54f82014de
Вероника Мамедова <mamedova.1999@inbox.ru>
2018-05-11 11:02:56 +0300

    ffrv

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba4
+{
+
+    class A
+    {
+        private int a; int b;
+        public A()
+        {
+            try
+            {
+                Console.WriteLine("Введите a");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите b");
+                b = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат данных");
+            }
+        }
+        public int c
+        {
+            get
+            {
+                if (a > b) return a;
+                else return b;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            Console.WriteLine("Значение с = {0}", obj.c);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laba3
+{
+    class Tchka
+    {
+        public int Koordinatax, Koordinatay;
+        public Tchka()
+        {
+            Koordinatax = Koordinatay = 0;
+        }
+        public void Position(int x, int y)
+        {
+
+            Koordinatax = x;
+            Koordinatay = y;
+        }
+    }
+    class ColorTchka : Tchka
+    {
+        public byte r;
+        public byte g;
+        public byte b;
+
+        public void Color(byte R, byte G, byte B)
+        {
+            r = R;
+            g = G;
+            b = B;
+        }
+        public int KoordinataX()
+        {
+            return Koordinatax;
+        }
+        public int KoordinataY()
+        {
+            return Koordinatay;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorTchka color = new ColorTchka();
+            color.Position(0, 0);
+            color.Color(17, 15, 0xFF);
+            Console.WriteLine("Координаты точки = ({0},{1})",
+                color.KoordinataX(), color.KoordinataY());
+            Console.WriteLine("Цвет точки =({0},{1},{2}) ", color.r, color.g, color.b);
+            Console.ReadLine();
+
+        }
+    }
+}
