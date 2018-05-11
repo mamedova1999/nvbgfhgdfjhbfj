91b691844e6257187b4fea797f66c952e6d8645a
Вероника Мамедова <mamedova.1999@inbox.ru>
2018-05-11 09:58:32 +0300

    hjhjgj

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class A
+    {
+        public decimal a;
+        public decimal b;
+        public A()
+        {
+            a = 10;
+            b = 8;
+        }
+        public A(decimal a, decimal b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public decimal c
+        {
+            get
+            {
+                if (a > b) return a;
+                else return b;
+            }
+        }
+    }
+    class B : A
+    {
+        public B(decimal a, decimal b) : base(a, b)
+        { }
+        public B()
+        {
+            d = 28;
+        }
+
+        public decimal d;
+        public decimal c2
+        {
+            get { return --a + --b + --d; }
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            Console.WriteLine("Значение с = {0}", obj.c);
+            B obj1 = new B();
+            Console.WriteLine("Значение с2 = {0}", obj1.c2);
+            Console.ReadKey();
+        }
+    }
+}
+
