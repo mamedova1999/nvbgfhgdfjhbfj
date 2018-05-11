5295ef78e33a3a45e89ca3f5f41a184ffc7472b6
Вероника Мамедова <mamedova.1999@inbox.ru>
2018-05-11 09:55:23 +0300

    hggh

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class A
+    {
+        private int a; int b;
+        public A()
+        {
+            a = 10;
+            b = -11;
+        }
+        public int c
+        {
+            get
+            {
+                if (a > b) return a;
+                else return b;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            Console.WriteLine("Значение с = {0}", obj.c);
+            Console.ReadKey();
+        }
+    }
+}
