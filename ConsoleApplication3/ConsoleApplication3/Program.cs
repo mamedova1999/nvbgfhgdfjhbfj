f71df722eb1aab1d9b941a548bdf00eed020af6c
Вероника Мамедова <mamedova.1999@inbox.ru>
2018-05-11 10:13:10 +0300

    jhghjghj

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+
+    public class Tochka
+    {
+        public Tochka(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public double X
+        {
+            get;
+            set;
+        }
+        public double Y
+        {
+            get;
+            set;
+        }
+    }
+    public class Krug
+    {
+        public double Radius
+        {
+            get;
+            set;
+        }
+        public Tochka Center
+        {
+            get;
+            set;
+        }
+        public Krug(double radius, Tochka center)
+        {
+            Radius = radius;
+            Center = center;
+        }
+        public double PlosadKruga
+        {
+            get { return Math.PI * Math.Pow(Radius, 2); }
+        }
+        public double DlinaOkruznosti
+        {
+            get { return Math.PI * 2 * Radius; }
+        }
+        public bool Vnutri(Tochka tochka)
+        {
+            Tochka vec = new Tochka(tochka.X - Center.X, tochka.Y - Center.Y);
+            double dist = Math.Pow(vec.X, 2) + Math.Pow(vec.Y, 2);
+            return dist <= Math.Pow(Radius, 2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводим координаты центра");
+            Tochka tochka = new Tochka(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+            Console.WriteLine("Координаты центра = {0},{1}", tochka.X, tochka.Y);
+            Console.WriteLine("Вводим координаты радиус окружности и координаты новой точки");
+            Krug mkrug = new Krug(double.Parse(Console.ReadLine()), tochka);
+            Console.WriteLine("Входит ли точка в окружность {0}", mkrug.Vnutri(new Tochka(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()))));
+            Console.WriteLine("Площадь = {0}", mkrug.PlosadKruga);
+            Console.WriteLine("Длина окружности = {0}", mkrug.DlinaOkruznosti);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleApplication3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ConsoleApplication3")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a894c1e6-116e-40b6-9b3f-5b430d60333d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
